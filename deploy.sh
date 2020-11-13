f0fa93a3df45c7ff0129ebf626e6ece815c42609
Tecty <z5141448@ad.unsw.edu.au>
2020-11-13 18:26:30 +0800

    the backend document

--- deploy.sh
+++ deploy.sh
@@ -3,4 +3,4 @@ mkdir els_data
 chmod 777 els_data
 docker-compose up -d --build
 cp avatar.jpg img/small/avatar.jpg
-sleep 5 && ./chore/enableEls.sh http://localhost:8000/els
+sleep 20 && ./chore/enableEls.sh http://localhost:8000/els
